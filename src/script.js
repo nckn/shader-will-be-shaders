523af94baf9000837875e5d5d4582f4dd0187645
Niels Christian Konrad Bisgaard <mail@nckn.dk>
2022-08-03 14:55:11 +0200

    Trying to implement ml5 poseNet

--- src/script.js
+++ src/script.js
@@ -23,7 +23,7 @@ let scene, matDrop;
 
 // ml5 - start
 let handpose;
-let video;
+// let video;
 let hands = [];
 // ml5 - end
 
@@ -473,3 +473,121 @@ const RippleEffect = (function () {
 })();
 
 const app = new App();
+
+// Copyright (c) 2019 ml5
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+/* ===
+ml5 Example
+PoseNet using p5.js
+=== */
+/* eslint-disable */
+
+// Grab elements, create settings, etc.
+var video = document.getElementById("video");
+console.log('video');
+console.log(video);
+var canvas = document.getElementById("canvasPose");
+var ctx = canvas.getContext("2d");
+
+// The detected positions will be inside an array
+let poses = [];
+
+// Create a webcam capture
+if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+  console.log('navigator.mediaDevices - 1')
+  navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+    console.log('navigator.mediaDevices - 2')
+    video.srcObject = stream;
+    video.play();
+  });
+}
+else {
+  // video.play();
+  document.body.addEventListener('click', _ => {
+    video.play();
+  })
+}
+
+// A function to draw the video and poses into the canvas.
+// This function is independent of the result of posenet
+// This way the video will not seem slow if poseNet
+// is not detecting a position
+function drawCameraIntoCanvas() {
+  // Draw the video element into the canvas
+  ctx.drawImage(video, 0, 0, 640, 480);
+  // We can call both functions to draw all keypoints and the skeletons
+  drawKeypoints();
+  drawSkeleton();
+
+  console.log("drawCameraIntoCanvas");
+
+  window.requestAnimationFrame(drawCameraIntoCanvas);
+}
+// Loop over the drawCameraIntoCanvas function
+drawCameraIntoCanvas();
+
+setTimeout(_ => {
+  // Create a new poseNet method with a single detection
+  const poseNet = ml5.poseNet(video, modelReady);
+  console.log('poseNet')
+  console.log(poseNet)
+  poseNet.on("pose", gotPoses);
+}, 1000)
+
+console.log('ml5')
+console.log(ml5)
+
+// A function that gets called every time there's an update from the model
+function gotPoses(results) {
+  poses = results;
+}
+
+function modelReady() {
+  console.log("model ready");
+  poseNet.multiPose(video);
+}
+
+// A function to draw ellipses over the detected keypoints
+function drawKeypoints() {
+  // Loop through all the poses detected
+  
+  for (let i = 0; i < poses.length; i += 1) {
+
+    console.log(poses[i]);
+
+    if (poses.pose['rightWrist']) {
+      console.log('rightWrist')
+      console.log(poses['rightWrist'])
+    }
+
+    // For each pose detected, loop through all the keypoints
+    for (let j = 0; j < poses[i].pose.keypoints.length; j += 1) {
+      let keypoint = poses[i].pose.keypoints[j];
+      // Only draw an ellipse is the pose probability is bigger than 0.2
+      if (keypoint.score > 0.2) {
+        ctx.beginPath();
+        ctx.arc(keypoint.position.x, keypoint.position.y, 10, 0, 2 * Math.PI);
+        ctx.stroke();
+      }
+    }
+  }
+}
+
+// A function to draw the skeletons
+function drawSkeleton() {
+  // Loop through all the skeletons detected
+  for (let i = 0; i < poses.length; i += 1) {
+    // For every skeleton, loop through all body connections
+    for (let j = 0; j < poses[i].skeleton.length; j += 1) {
+      let partA = poses[i].skeleton[j][0];
+      let partB = poses[i].skeleton[j][1];
+      ctx.beginPath();
+      ctx.moveTo(partA.position.x, partA.position.y);
+      ctx.lineTo(partB.position.x, partB.position.y);
+      ctx.stroke();
+    }
+  }
+}
