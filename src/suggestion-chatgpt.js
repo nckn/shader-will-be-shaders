93ccff5e80e47ff599dd86888633647ef8946136
Niels Konrad <mail@konradstudio.com>
2023-05-23 23:25:42 +0200

    trying to distort mesh

--- src/suggestion-chatgpt.js
+++ src/suggestion-chatgpt.js
@@ -1,21 +1,5 @@
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-
-// import ASScroll from '@ashthornton/asscroll'
-// import GSAP from 'gsap'
-// import { map } from '../static/js/math'
-// import { split } from '../static/js/text'
-
-import './assets/scss/index.scss'
-
-// Misc helper functions
-// import {
-//   checkIfTouch,
-//   map,
-//   createPoints
-// } from '../static/js/helpers.js'
-// Longpress
-// import LongPress from '../static/js/LongPress.js'
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 let scene, matDrop;
 
@@ -26,47 +10,25 @@ function App() {
     cameraZ: 100,
   };
 
-  let renderer, scene, camera, cameraCtrl;
+  let renderer, camera, cameraCtrl;
   let width, height, cx, cy, wWidth, wHeight;
 
   let ripple;
   let gridWWidth, gridWHeight;
   let gridWidth, gridHeight;
 
-  // Should we show the grid lines or not?
-  let should_draw_lines = false
-  // The mesh itself
-  let thePlane = null
-
-  const mouse = new THREE.Vector2();
-  const mousePlane = new THREE.Plane(new THREE.Vector3(0, 0, 1), 0);
-  const mousePosition = new THREE.Vector3();
-  const raycaster = new THREE.Raycaster();
-  let mouseOver = false;
+  let thePlane = null;
 
   init();
 
   function init() {
-
-    console.log('initing alright')
-    // const gl = renderer.getContext();
-    // const floatTextures = gl.getExtension('OES_texture_float');
-    // if (!floatTextures) {
-    //   alert('no floating point texture support');
-    //   return;
-    // }
-
     renderer = new THREE.WebGLRenderer({ canvas: document.getElementById(conf.el), antialias: true });
     camera = new THREE.PerspectiveCamera(conf.fov);
     camera.position.x = 20;
-    // camera.position.z = conf.cameraZ;
-    // camera.lookAt(new THREE.Vector3(0,1,0))
 
     updateSize();
     window.addEventListener('resize', updateSize, false);
 
-    // gridWHeight = wHeight - 20;
-    // gridWWidth = gridWHeight;
     gridWHeight = wHeight;
     gridWWidth = wWidth;
     gridWidth = gridWWidth * width / wWidth;
@@ -78,24 +40,22 @@ function App() {
       const v = new THREE.Vector3();
       camera.getWorldDirection(v);
       v.normalize();
+      const mouse = new THREE.Vector2();
       mouse.x = ((e.clientX / width) * 2 - 1);
       mouse.y = (-(e.clientY / height) * 2 + 1);
+      const raycaster = new THREE.Raycaster();
       raycaster.setFromCamera(mouse, camera);
+      const mousePlane = new THREE.Plane(new THREE.Vector3(0, 0, 1), 0);
+      const mousePosition = new THREE.Vector3();
       raycaster.ray.intersectPlane(mousePlane, mousePosition);
       return { x: 2 * mousePosition.x / gridWWidth, y: 2 * mousePosition.y / gridWHeight };
     };
 
     renderer.domElement.addEventListener('mousemove', e => {
-      mouseOver = true;
       const gp = getGridMP(e);
       ripple.addDrop(gp.x, gp.y, 0.05, 0.1);
     });
-    renderer.domElement.addEventListener('mouseleave', e => { mouseOver = false; });
-
-    // renderer.domElement.addEventListener('mouseup', e => {
-    //   const gp = getGridMP(e);
-    //   ripple.addDrop(gp.x, gp.y, 0.2, -3.0);
-    // });
+    renderer.domElement.addEventListener('mouseleave', e => { ripple.clearDrops(); });
 
     initScene();
     animate();
@@ -104,18 +64,16 @@ function App() {
   function initScene() {
     scene = new THREE.Scene();
 
-    let pointLight1 = new THREE.PointLight(0xFFFF80);
+    const pointLight1 = new THREE.PointLight(0xFFFF80);
     pointLight1.position.set(-wWidth / 2, wHeight / 2, 50);
     scene.add(pointLight1);
 
-    // Make a plane
-    const geometry = new THREE.PlaneBufferGeometry(wWidth, wHeight, 100, 100);
-    const materialPlane = new THREE.MeshStandardMaterial({
+    const geometry = new THREE.PlaneBufferGeometry(wWidth, wHeight);
+    const material = new THREE.MeshStandardMaterial({
       color: 0xffffff,
       side: THREE.DoubleSide,
       metalness: 0.5,
       roughness: 0.5,
-      wireframe: true,
       onBeforeCompile: shader => {
         shader.uniforms.hmap = { value: ripple.hMap.texture };
         shader.vertexShader = `
@@ -132,7 +90,7 @@ function App() {
             
             vUv = uv;
           }
-        `;
+        ` + shader.vertexShader;
         
         shader.fragmentShader = `
           uniform sampler2D hmap;
@@ -146,28 +104,22 @@ function App() {
             
             gl_FragColor = vec4(shiftedColor, 1.0);
           }
-        `;
+        ` + shader.fragmentShader;
       }
     });
 
-    thePlane = new THREE.Mesh(geometry, materialPlane);
+    thePlane = new THREE.Mesh(geometry, material);
     scene.add(thePlane);
-    // const geometry = new THREE.PlaneBufferGeometry(wWidth, wHeight);
-    // // Make a sphere
-    // // const geometry = new THREE.SphereBufferGeometry(wWidth, wWidth, wWidth);
-    // thePlane = new THREE.Mesh(geometry, matDrop)
-    // // Add new mesh
-    // scene.add( thePlane )
 
-    let pointLight2 = new THREE.PointLight(0xde3578);
+    const pointLight2 = new THREE.PointLight(0xde3578);
     pointLight2.position.set(wWidth / 2, wHeight / 2, 50);
     scene.add(pointLight2);
 
-    let pointLight3 = new THREE.PointLight(0xFF4040);
+    const pointLight3 = new THREE.PointLight(0xFF4040);
     pointLight3.position.set(-wWidth / 2, -wHeight / 2, 50);
     scene.add(pointLight3);
 
-    let pointLight4 = new THREE.PointLight(0x0247e5);
+    const pointLight4 = new THREE.PointLight(0x0247e5);
     pointLight4.position.set(wWidth / 2, -wHeight / 2, 50);
     scene.add(pointLight4);
 
@@ -178,62 +130,6 @@ function App() {
       pointLight4.color = new THREE.Color(chroma.random().hex());
     });
 
-    const material = new THREE.MeshStandardMaterial({
-      color: 0xffffff, side: THREE.DoubleSide, metalness: 0.5, roughness: 0.5, onBeforeCompile: shader => {
-        shader.uniforms.hmap = { value: ripple.hMap.texture };
-        shader.vertexShader = "uniform sampler2D hmap;\n" + shader.vertexShader;
-        const token = '#include <begin_vertex>';
-        const customTransform = `
-        vec3 transformed = vec3(position);
-        vec4 info = texture2D(hmap, uv);
-        vNormal = vec3(info.b, sqrt(1.0 - dot(info.ba, info.ba)), info.a).xzy;
-        transformed.z = 20. * info.r;
-      `;
-        shader.vertexShader = shader.vertexShader.replace(token, customTransform);
-      }
-    });
-
-    let nx = Math.round(gridWidth / 2), ny = Math.round(gridHeight / 20);
-    let dx = gridWWidth / nx, dy = gridWHeight / ny;
-    for (let j = 0; j <= ny; j++) {
-      const geometry = new THREE.BufferGeometry();
-      const positions = [], uvs = [];
-      const y = - gridWHeight / 2 + j * dy;
-      for (let i = 0; i <= nx; i++) {
-        positions.push(- gridWWidth / 2 + i * dx, y, 0);
-        uvs.push(i / nx, j / ny);
-      }
-      geometry.setAttribute('position', new THREE.Float32BufferAttribute(positions, 3));
-      geometry.setAttribute('uv', new THREE.Float32BufferAttribute(uvs, 2));
-      geometry.computeBoundingSphere();
-
-
-      // Draw lines on x axis
-      if (should_draw_lines) {
-        scene.add(new THREE.Line(geometry, material));
-      }
-    }
-
-    nx = Math.round(gridWidth / 20); ny = Math.round(gridHeight / 2);
-    dx = gridWWidth / nx; dy = gridWHeight / ny;
-    for (let i = 0; i <= nx; i++) {
-      const geometry = new THREE.BufferGeometry();
-      const positions = [], uvs = [];
-      const x = - gridWWidth / 2 + i * dx;
-      for (let j = 0; j <= ny; j++) {
-        positions.push(x, - gridWHeight / 2 + j * dy, 0);
-        uvs.push(i / nx, j / ny);
-      }
-      geometry.setAttribute('position', new THREE.Float32BufferAttribute(positions, 3));
-      geometry.setAttribute('uv', new THREE.Float32BufferAttribute(uvs, 2));
-      geometry.computeBoundingSphere();
-
-      // Draw lines on y axis
-      if (should_draw_lines) {
-        scene.add(new THREE.Line(geometry, material));
-      }
-    }
-    // camera.position.set(0, -gridWHeight / 2, 100);
     camera.position.set(0, 0, 100);
     camera.lookAt(new THREE.Vector3(0, -gridWHeight / 6, 0));
 
@@ -244,28 +140,22 @@ function App() {
   }
 
   function animate() {
-    if (!mouseOver) {
-      const time = Date.now() * 0.001;
-      const x = Math.cos(time) * 0.2;
-      const y = Math.sin(time) * 0.2;
-      ripple.addDrop(x, y, 0.05, -0.04);
-    }
-
     ripple.update();
     renderer.render(scene, camera);
     requestAnimationFrame(animate);
   }
 
   function updateSize() {
-    width = window.innerWidth; cx = width / 2;
-    height = window.innerHeight; cy = height / 2;
+    width = window.innerWidth;
+    cx = width / 2;
+    height = window.innerHeight;
+    cy = height / 2;
     renderer.setSize(width, height);
     camera.aspect = width / height;
     camera.updateProjectionMatrix();
     const wsize = getRendererSize();
-    wWidth = wsize[0]; wHeight = wsize[1];
-
-    console.log('on resize')
+    wWidth = wsize[0];
+    wHeight = wsize[1];
   }
 
   function getRendererSize() {
@@ -282,7 +172,6 @@ const RippleEffect = (function () {
     this.renderer = renderer;
     this.width = 512;
     this.height = 512;
-    // this.delta = new THREE.Vector2(this.width / Math.pow(width, 2), this.height / Math.pow(height, 2));
     this.delta = new THREE.Vector2(1 / this.width, 1 / this.height);
 
     this.hMap = new THREE.WebGLRenderTarget(this.width, this.height, { type: THREE.FloatType, depthBuffer: false, stencilBuffer: false });
@@ -293,7 +182,6 @@ const RippleEffect = (function () {
   }
 
   RippleEffect.prototype.initShaders = function () {
-    // default vertex shader
     const defaultVertexShader = `
       varying vec2 vUv;
       void main() {
@@ -344,26 +232,6 @@ const RippleEffect = (function () {
       `,
     });
 
-    this.normalsMat = new THREE.ShaderMaterial({
-      uniforms: {
-        'tDiffuse': { value: null },
-        'delta': new THREE.Uniform(this.delta),
-      },
-      vertexShader: defaultVertexShader,
-      fragmentShader: `
-      uniform sampler2D tDiffuse;
-      uniform vec2 delta;
-      varying vec2 vUv;
-      void main() {
-        vec4 texel = texture2D(tDiffuse, vUv);
-        vec3 dx = vec3(delta.x, texture2D(tDiffuse, vec2(vUv.x + delta.x, vUv.y)).r - texel.r, 0.0);
-        vec3 dy = vec3(0.0, texture2D(tDiffuse, vec2(vUv.x, vUv.y + delta.y)).r - texel.r, delta.y);
-        texel.ba = normalize(cross(dy, dx)).xz;
-        gl_FragColor = texel;
-      }
-    `,
-    });
-
     this.dropMat = new THREE.ShaderMaterial({
       uniforms: {
         'tDiffuse': { value: null },
@@ -384,29 +252,19 @@ const RippleEffect = (function () {
           float drop = max(0.0, 1.0 - length(center * 0.5 + 0.5 - vUv) / radius);
           drop = 0.5 - cos(drop * PI) * 0.5;
           texel.r += drop * strength;
-          // texel.r = clamp(texel.r, -2.0, 2.0);
           gl_FragColor = texel;
         }
       `,
     });
-
-    matDrop = this.dropMat
   };
 
   RippleEffect.prototype.update = function () {
     this.updateHMap();
-    this.updateHMapNormals();
   };
 
   RippleEffect.prototype.updateHMap = function () {
     this.updateMat.uniforms.tDiffuse.value = this.hMap.texture;
     this.renderShaderMat(this.updateMat, this.hMap1);
-    this.swapBuffers();
-  };
-
-  RippleEffect.prototype.updateHMapNormals = function () {
-    this.normalsMat.uniforms.tDiffuse.value = this.hMap.texture;
-    this.renderShaderMat(this.normalsMat, this.hMap1);
     this.swapBuffers();
   };
 
@@ -417,6 +275,11 @@ const RippleEffect = (function () {
     this.dropMat.uniforms.strength.value = strength;
     this.renderShaderMat(this.dropMat, this.hMap1);
     this.swapBuffers();
+  };
+
+  RippleEffect.prototype.clearDrops = function () {
+    this.hMap1 = new THREE.WebGLRenderTarget(this.width, this.height, { type: THREE.FloatType, depthBuffer: false, stencilBuffer: false });
+    this.hMap = new THREE.WebGLRenderTarget(this.width, this.height, { type: THREE.FloatType, depthBuffer: false, stencilBuffer: false });
   };
 
   RippleEffect.prototype.renderBuffer = function (buffer, target) {
@@ -439,7 +302,6 @@ const RippleEffect = (function () {
     this.hMap1 = temp;
   };
 
-  // from https://threejs.org/examples/js/postprocessing/EffectComposer.js
   const FullScreenQuad = (function () {
     const camera = new THREE.OrthographicCamera(- 1, 1, 1, - 1, 0, 1);
     const geometry = new THREE.PlaneBufferGeometry(2, 2);
